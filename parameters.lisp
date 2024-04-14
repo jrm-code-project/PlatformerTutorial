c93f3c4c658e74e85bdf7528650ffc81192baf3c
Joe Marshall <joseph.marshall@nutanix.com>
2024-04-14 05:29:55 -0700

    Fixup.

--- parameters.lisp
+++ parameters.lisp
@@ -6,8 +6,5 @@
 (defun scalef (quantity) (* (game-scale) quantity))
 (defun scale  (quantity) (floor (scalef quantity)))
 
-(defun width-in-tiles () 26)
-(defun height-in-tiles () 14)
-(defun-scaled tile-size 32)
-(defun game-width () (* (tile-size) (width-in-tiles)))
-(defun game-height () (* (tile-size) (height-in-tiles)))
+(defun game-width () 640)
+(defun game-height () 480)
