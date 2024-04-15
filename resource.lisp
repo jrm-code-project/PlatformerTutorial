1f3e055d6b4338aa35a9a68d2af2ff24f33bd8a7
Joe Marshall <joseph.marshall@nutanix.com>
2024-04-14 21:29:15 -0700

    Fixup.

--- resource.lisp
+++ resource.lisp
@@ -79,7 +79,7 @@
                    `(PROGN ,@(cdr binding)))
            bindings)))
 
-(defmethod call-with-surfaces ((game platformer) receiver)
+(defun call-with-surfaces (receiver)
   (let-surfaces ((button-atlas-surface        (resource-pathname "button_atlas.png"))
                  (menu-surface                (resource-pathname "menu_background.png"))
                  (menu-background-surface     (resource-pathname "background_menu.png"))
@@ -102,7 +102,6 @@
 
 (defmacro with-surfaces ((surfaces) &body body)
   `(CALL-WITH-SURFACES
-    ,game
     (LAMBDA (,surfaces)
       ,@body)))
 
